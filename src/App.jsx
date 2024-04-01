702cf1ef4421c930850c4de9bb175f697e51413f
zzannat <zzannat@my.centennialcollege.ca>
2024-04-01 19:03:49 -0400

    removed excess files

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import StudentApp1 from "react_app1/StudentApp1";
 
-import {BrowserRouter , Route,  Routes,  Switch } from "react-router-dom";
 
 
 const App = () =>
 {
   return(
     <StudentApp1/>
-  // <BrowserRouter>
-  //   <Routes>
-  //   <Route exact path ="/student2" element={<StudentApp2 />}>
-  //   </Route>
-  //   <Route exact path="/student3" element={<StudentApp3 />}>
-  //   </Route>
-  //   </Routes>
-  // </BrowserRouter>
+  
   )
   
 };
--- src/StudentApp3.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import StudentApp1 from "react_app1/StudentApp1";
-
-
-export default function StudentApp3(){
-  return(
-    <StudentApp1/>
-  );
-}
