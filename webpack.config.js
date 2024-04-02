df17447732de79b389731ab623287e9fa0420c34
zzannat <zzannat@my.centennialcollege.ca>
2024-04-02 16:12:02 -0400

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const Dotenv = require('dotenv-webpack');
 const deps = require("./package.json").dependencies;
 module.exports = (_, argv) => ({
   output: {
-    //publicPath: "https://reactmicroservicepp2.netlify.app/",
+    publicPath: "https://reactmicroservicepp2.netlify.app/",
   },
 
   resolve: {
